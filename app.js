a29694deac6de5628acbcefeb0a014828f89d6cb
ramiouse <43832932+ramiouse@users.noreply.github.com>
2022-10-29 12:05:02 +0700

    fdssd

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var cors = require("cors");
 var app = express();
 
 // used for anticipate access origin erro
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 // app.use(settingProxy);
 
 // --------------------------------------------------- CORS SETTINGS
